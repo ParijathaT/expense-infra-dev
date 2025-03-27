8f192c94d4e604847737351776f746dd30dc1bd5
parijathaT <parijatha10440@gmail.com>
2025-03-28 00:48:24 +0530

    expense-infra

--- 10-sg/sg.tf
+++ 10-sg/sg.tf
@@ -1,8 +1,20 @@
 module "mysql_sg" {
-    source = "../terraform-aws-securitygroup"
+    #source = "../terraform-aws-securitygroup"
+    source = "git::https://github.com/ParijathaT/terraform-aws-securitygroup.git?ref=main"
     project_name = var.project_name
     environment = var.environment
     sg_name = "mysql"
+    sg_description = "Created for MySQL instances in expense dev"
+    vpc_id = data.aws_ssm_parameter.vpc_id.value
+    common_tags = var.common_tags
+}
+
+module "backend_sg" {
+    #source = "../terraform-aws-securitygroup"
+    source = "git::https://github.com/ParijathaT/terraform-aws-securitygroup.git?ref=main"
+    project_name = var.project_name
+    environment = var.environment
+    sg_name = "backend"
     sg_description = "Created for MySQL instances in expense dev"
     vpc_id = data.aws_ssm_parameter.vpc_id.value
     common_tags = var.common_tags
