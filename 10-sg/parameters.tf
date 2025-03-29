c3f2c92dbbe82e6bf3e97b01826230be5bb08195
parijathaT <parijatha10440@gmail.com>
2025-03-29 16:11:34 +0530

    expense infa

--- 10-sg/parameters.tf
+++ 10-sg/parameters.tf
@@ -3,3 +3,27 @@ resource "aws_ssm_parameter" "mysql_sg_id" {
   type  = "String"
   value = module.mysql_sg.sg_id
 }
+
+resource "aws_ssm_parameter" "backend_sg_id" {
+  name  = "/${var.project_name}/${var.environment}/backend_sg_id"
+  type  = "String"
+  value = module.backend_sg.sg_id
+}
+
+resource "aws_ssm_parameter" "frontend_sg_id" {
+  name  = "/${var.project_name}/${var.environment}/frontend_sg_id"
+  type  = "String"
+  value = module.frontend_sg.sg_id
+}
+
+resource "aws_ssm_parameter" "bastian_sg_id" {
+  name  = "/${var.project_name}/${var.environment}/bastian_sg_id"
+  type  = "String"
+  value = module.bastian_sg.sg_id
+}
+
+resource "aws_ssm_parameter" "app_alb_sg_id" {
+  name  = "/${var.project_name}/${var.environment}/app_alb_sg_id"
+  type  = "String"
+  value = module.app_alb_sg.sg_id
+}
--- 10-sg/sg.tf
+++ 10-sg/sg.tf
@@ -19,3 +19,36 @@ module "backend_sg" {
     vpc_id = data.aws_ssm_parameter.vpc_id.value
     common_tags = var.common_tags
 }
+
+module "frontend_sg" {
+    #source = "../terraform-aws-securitygroup"
+    source = "git::https://github.com/ParijathaT/terraform-aws-securitygroup.git?ref=main"
+    project_name = var.project_name
+    environment = var.environment
+    sg_name = "frontend"
+    sg_description = "Created for MySQL instances in expense dev"
+    vpc_id = data.aws_ssm_parameter.vpc_id.value
+    common_tags = var.common_tags
+}
+
+module "bastian_sg" {
+    #source = "../terraform-aws-securitygroup"
+    source = "git::https://github.com/ParijathaT/terraform-aws-securitygroup.git?ref=main"
+    project_name = var.project_name
+    environment = var.environment
+    sg_name = "bastian"
+    sg_description = "Created for bastian instances in expense dev"
+    vpc_id = data.aws_ssm_parameter.vpc_id.value
+    common_tags = var.common_tags
+}
+
+module "app-alb_sg" {
+    #source = "../terraform-aws-securitygroup"
+    source = "git::https://github.com/ParijathaT/terraform-aws-securitygroup.git?ref=main"
+    project_name = var.project_name
+    environment = var.environment
+    sg_name = "app-alb"
+    sg_description = "Created for backend alb instances in expense dev"
+    vpc_id = data.aws_ssm_parameter.vpc_id.value
+    common_tags = var.common_tags
+}
