48ca560f39e8867e0c1f5debc8a8f1d5ce1e8fc5
parijathaT <parijatha10440@gmail.com>
2025-04-14 00:29:50 +0530

    expense-infra

--- 00-vpc/output.tf
+++ 00-vpc/output.tf
@@ -2,6 +2,6 @@
 #     value = module.vpc.azs_info
 # }
 
-# output "public_subnet_ids"{
-#     value = module.vpc.public_subnet_ids
-# }
+output "public_subnet_ids"{
+    value = module.vpc.public_subnet_ids
+}
--- 00-vpc/provider.tf
+++ 00-vpc/provider.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "5.84.0"
+      version = "5.93.0"
     }
   }
 
@@ -11,7 +11,7 @@ terraform {
     bucket = "parijatha-tf-remote-state-dev"
     key    = "expense-dev-vpc" # you should have unique keys with in the bucket, same key should not be used in other repos or tf projects
     region = "us-east-1"
-    dynamodb_table = "82s-tf-remote-state-dev"
+    dynamodb_table = "82s-tf-remote-state-lock-dev"
   }
 }
 
