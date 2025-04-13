eeb53793e3d5657e448cd8c154a6c80698b33fcb
parijathaT <parijatha10440@gmail.com>
2025-04-14 00:31:57 +0530

    expense-infra

---
+++ 60-backend/backend.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+dnf install ansible -y
+# push
+# ansible-playbook -i inventory mysql.yaml
+
+#pull
+ansible-pull  -i localhost, -U https://github.com/DAWS-82S/expense-ansible-roles-tf.git main.yaml -e COMPONENT=backend -e ENVIRONMENT=$1
+git@github.com:ParijathaT/expense-ansible-roles.git
