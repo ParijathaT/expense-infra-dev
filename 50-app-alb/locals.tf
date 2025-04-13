6e8ecfea2041f96fb169a980bca5da2f23007e98
parijathaT <parijatha10440@gmail.com>
2025-04-14 00:31:43 +0530

    expense-infra

--- 50-app-alb/locals.tf
+++ 50-app-alb/locals.tf
@@ -1,6 +1,5 @@
-
-locals{
-   public_subnet_ids = split("," , data.aws_ssm_parameter.public_subnet_ids.value)
-   app_alb_sg_id = data.aws_ssm_parameter.app_alb_sg_id.value
+locals {
+  resource_name = "${var.project_name}-${var.environment}"
+  mysql_sg_id = data.aws_ssm_parameter.mysql_sg_id.value
+  database_subnet_group_name = data.aws_ssm_parameter.database_subnet_group_name.value
 }
- 
--- 50-app-alb/variables.tf
+++ 50-app-alb/variables.tf
@@ -13,3 +13,11 @@ variable "common_tags"{
         Terraform = "true"
     }
 }
+
+variable "zone_id" {
+    default = "Z02554302X11M342L99S3"
+}
+
+variable "domain_name" {
+    default = "parijathapractice.online"
+}
